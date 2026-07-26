0656cf084cdc143c31f0f4427256ceaa10cd7af1
Josh Rendek <josh@bluescripts.net>
2026-07-26 16:16:44 -0400

    test: exercise country trust production wiring

---
+++ cmd/manyforge/analytics_country_wiring_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/manyforge/manyforge/internal/platform/config"
+)
+
+func TestAnalyticsCountryTrustProductionWiring(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		env  string
+		want bool
+	}{
+		{name: "enabled", env: "true", want: true},
+		{name: "disabled by default", env: "", want: false},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Setenv("MANYFORGE_TRUST_CF_IPCOUNTRY", tc.env)
+			cfg, err := config.Load()
+			if err != nil {
+				t.Fatalf("config.Load: %v", err)
+			}
+
+			handler := newAnalyticsPublicHandler(nil, nil, nil, nil, cfg)
+			if handler.TrustCloudflareCountryHeader != tc.want {
+				t.Fatalf("TrustCloudflareCountryHeader = %t, want %t",
+					handler.TrustCloudflareCountryHeader, tc.want)
+			}
+		})
+	}
+}
